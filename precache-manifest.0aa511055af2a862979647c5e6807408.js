320a58364dbc2a53f6e7e6bd75a6a2303d9e7d82
TheLaurenGrier <56608005+TheLaurenGrier@users.noreply.github.com>
2022-12-16 08:46:36 -0500

    Updates

---
+++ precache-manifest.0aa511055af2a862979647c5e6807408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffaa8ea5590ae420fa32",
+    "url": "/robofriends/static/css/main.e228ada3.chunk.css"
+  },
+  {
+    "revision": "ffaa8ea5590ae420fa32",
+    "url": "/robofriends/static/js/main.742f329f.chunk.js"
+  },
+  {
+    "revision": "877f8ad29b66b8d6fe13",
+    "url": "/robofriends/static/js/runtime~main.f8b0d496.js"
+  },
+  {
+    "revision": "ef61d54630c2071878f8",
+    "url": "/robofriends/static/js/2.e71dca24.chunk.js"
+  },
+  {
+    "revision": "eac2663a493c35f8869d",
+    "url": "/robofriends/static/js/3.a2d6af6c.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5172ebab32919c63efae8c37f80168c6",
+    "url": "/robofriends/index.html"
+  }
+];
