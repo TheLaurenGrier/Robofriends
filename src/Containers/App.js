b7ba59d2f0810ef47fbaac38648ceb5021d5f825
TheLaurenGrier <56608005+TheLaurenGrier@users.noreply.github.com>
2022-12-16 08:38:47 -0500

    added ErrorBoundry
    Added ErrorBoundry to CardList

---
+++ src/Containers/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import '../Containers/App.css';
+import Scroll from '../Components/Scroll';
+import ErrorBoundry from '../Components/ErrorBoundry'
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}));
+                
+    }
+
+    onSearchChange = (event) => {
+        this.setState ({ searchfield: event.target.value})
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+
+        if (this.state.robots.length === 0) {
+
+            return <h1> Loading</h1>
+        } else {
+            return (
+            <div className='tc'>
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                    <CardList robots= { filteredRobots }/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>    
+        );
+        }
+    }
+}
+export default App;
